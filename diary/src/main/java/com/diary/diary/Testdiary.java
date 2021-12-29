631e6888269ee1bc31ac597fe6a33e109da597ba
tnatna0801 <min120926@gmail.com>
2021-12-29 17:55:53 +0900

    프로젝트 생성

---
+++ diary/src/main/java/com/diary/diary/Testdiary.java
@@ -0,0 +1,15 @@
+package com.diary.diary;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Testdiary {
+
+    private final String user_id;
+    private final String email;
+    private final String pwd;
+
+
+}
