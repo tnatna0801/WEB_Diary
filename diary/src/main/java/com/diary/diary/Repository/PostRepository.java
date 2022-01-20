7bed3f75d7b5807b2008556aa01f802267a90bf8
tnatna0801 <min120926@gmail.com>
2022-01-20 18:24:06 +0900

    existsById 수정

--- diary/src/main/java/com/diary/diary/Repository/PostRepository.java
+++ diary/src/main/java/com/diary/diary/Repository/PostRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post, String> {
 
     Post findById(long id);
+
+    boolean existsById(long id);
 }
